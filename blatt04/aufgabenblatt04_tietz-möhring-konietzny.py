0be1fcbdd69ca38f3ccdc2918208295c1266a642
Carolin Konietzny <3konietz@informatik.uni-hamburg.de>
2015-11-08 20:28:15 +0100

    Aufgabenblatt 4

--- blatt04/aufgabenblatt04_tietz-möhring-konietzny.py
+++ blatt04/aufgabenblatt04_tietz-möhring-konietzny.py
@@ -12,6 +12,11 @@ class Nodes(object):
         self.h = None
         self.pos = None
 
+    def status(self):
+        if self.blocked: return "x"
+        if self.goal: return "g"
+        if self.prev: return "b"
+        return " "
     def __lt__(self, other):
         return True
 
@@ -32,6 +37,22 @@ def load_environment(filename):
         
         # Für jedes Feld des Environments einen leeren Knoten erstellen
         env = [[Nodes() for x in range(width)] for y in range(height)]
+        
+        #Portale werden in ein Dictionary geschrieben.
+        #Hierbei werden sie auf den selben Knoten gestetzt.
+        portals = dict()
+        for y in range(height):
+            for x in range(width):
+                symbol = lines[y][x]
+                if symbol in "1234567890":
+                    if symbol in portals:
+                        env[y][x] = portals[symbol]
+                    else:
+                        portals[symbol] = env[y][x]
+
+
+
+        
         start = None
         goal_pos = None
         #Variablen der Knoten belegen, hierbei die Position des Zielknotens merken, um im nächsten Durchlauf dann die Heuristik berechen
@@ -62,19 +83,29 @@ def load_environment(filename):
             for x in range(width):
                 dx = x - goal_pos[0]
                 dy = y - goal_pos[1]
-                env[y][x].h = math.sqrt(dx ** 2 + dy ** 2)
-        #lines wird zurückgegeben, um später bei der ausgabe das Fezld generieren zu können
-        return start, lines
+                #Falls es Portale gibt, wird keine Heuristik verwendet --> der Algorithmus ist Breitensuche
+                if portals:
+                    h = 0
+                else:
+                    h = math.sqrt(dx ** 2 + dy ** 2)
+                if env[y][x].h is None or env[y][x].h > h:
+                    env[y][x].h = h
+        #lines wird zurückgegeben, um später bei der ausgabe das Feld generieren zu können
+        return start, lines, env
 
 
 
-def astar(start):
+def astar(start, env):
+    #frontier_inserstions zählt die Operationen, frontier_max_size die größte vorkommende Frontier
+    frontier_insertions = 1
+    frontier_max_size = 1
     start.cost = 0
     frontier = PriorityQueue()
     frontier.put((0, start))
     while frontier.qsize() != 0 :
         node = frontier.get()[1]
         #print("Visiting node " + node.name)
+        #print("\n".join(["".join(node.status() for node in line) for line in env]))
         
         #Frontier erweitern, hierbei nur solche Knoten aufnehmen, die besucht werden dürfen (kein X) und noch nie in der Frontier waren
         #(ansonsten wäre cost gesetzt)
@@ -83,18 +114,25 @@ def astar(start):
                 neighbour.prev = node
                 neighbour.cost = node.cost + 1
                 frontier.put(( neighbour.cost + neighbour.h , neighbour))
+                frontier_insertions += 1
+                if frontier_max_size < frontier.qsize():
+                    frontier_max_size = frontier.qsize()
                 if neighbour.goal:
+                    print("Time: {} Memory: {}".format(frontier_insertions, frontier_max_size))
                     return neighbour
 
-#Fehlermeldung, falls der Skriptaufruf one Argument erfolgte (die Feld-Textdatei)
+    print("Time: {} Memory: {}".format(frontier_insertions, frontier_max_size))
+
+
+#Fehlermeldung, falls der Skriptaufruf ohne Argument erfolgte (die Feld-Textdatei)
 if len(sys.argv) < 2:
     print("Usage: {} FILE".format(sys.argv[0]))
     sys.exit(1)
 
 
-start, field = load_environment(sys.argv[1])
+start, field, env = load_environment(sys.argv[1])
 
-goal = astar(start)
+goal = astar(start, env)
 current = goal
 
 #Backtrace. Vom Zielknoten an werden die Felder mit einem . markiert, die den besten Pfad bilden.
